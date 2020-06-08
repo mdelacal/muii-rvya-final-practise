69d4366f0ac0f847cef42ca0d788d85388473bd1
Miguel de la Cal Bravo <miguelcal97@gmail.com>
2020-06-08 17:48:28 +0200

    Added Multimarker files & Modified practica.c and Makefile

--- proyecto_final/practica3/practica.c
+++ proyecto_final/practica3/practica.c
@@ -10,18 +10,19 @@
 #include <AR/video.h>   
 #include <AR/param.h>   
 #include <AR/ar.h>
+#include <AR/arMulti.h>
 
 // ==== Definicion de constantes y variables globales ===============
 int    patt_id;            // Identificador unico de la marca
 double patt_trans[3][4];   // Matriz de transformacion de la marca
+ARMultiMarkerInfoT *mMarker;       // Estructura global Multimarca
+int dmode = 0;   // Modo dibujo (objeto centrado o cubos en marcas)
 
 void print_error (char *error) {  printf(error); exit(0); }
 
 // ======== cleanup =================================================
-static void cleanup(void) {   
-  arVideoCapStop();            // Libera recursos al salir ...
-  arVideoClose();
-  argCleanup();
+static void cleanup(void) {   // Libera recursos al salir ...
+  arVideoCapStop();   arVideoClose();   argCleanup();   exit(0);
 }
 
 // ==== Definicion de estructuras ===================================
@@ -66,7 +67,7 @@ static void keyboard(unsigned char key, int x, int y) {
   // Recogemos los eventos de pulsaciones de teclas del teclado
   case 0x1B: case 'Q': case 'q':
     cleanup(); break;
-
+  case 'D': case'd': if (dmode == 0) dmode=1; else dmode=0; break;
   }
 }
 
@@ -101,6 +102,50 @@ static void draw( void ) {
   }
 }
 
+// ======== draw multimarca =========================================
+static void draw_multi( void ) {
+  double  gl_para[16];   // Esta matriz 4x4 es la usada por OpenGL
+  GLfloat material[]        = {1.0, 1.0, 1.0, 1.0};
+  GLfloat light_position[]  = {100.0,-200.0,200.0,0.0};
+  int i;
+  
+  argDrawMode3D();              // Cambiamos el contexto a 3D
+  argDraw3dCamera(0, 0);        // Y la vista de la camara a 3D
+  glClear(GL_DEPTH_BUFFER_BIT); // Limpiamos buffer de profundidad
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+
+  argConvGlpara(mMarker->trans, gl_para);   
+  glMatrixMode(GL_MODELVIEW);           
+  glLoadMatrixd(gl_para);               
+
+  glEnable(GL_LIGHTING);  glEnable(GL_LIGHT0);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  
+  if (dmode == 0) {  // Dibujar cubos en marcas
+    for(i=0; i < mMarker->marker_num; i++) {
+      glPushMatrix();   // Guardamos la matriz actual
+      argConvGlpara(mMarker->marker[i].trans, gl_para);   
+      glMultMatrixd(gl_para);               
+      if(mMarker->marker[i].visible < 0) {  // No se ha detectado
+	material[0] = 1.0; material[1] = 0.0; material[2] = 0.0; }
+      else {           // Se ha detectado (ponemos color verde)
+	material[0] = 0.0; material[1] = 1.0; material[2] = 0.0; }
+      glMaterialfv(GL_FRONT, GL_AMBIENT, material);
+      glTranslatef(0.0, 0.0, 25.0);
+      glutSolidCube(50.0);
+      glPopMatrix();   // Recuperamos la matriz anterior
+    }
+  } else { // Dibujar objeto global
+    glMaterialfv(GL_FRONT, GL_AMBIENT, material);
+    glTranslatef(150.0, -100.0, 60.0);
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+    glutSolidTeapot(90.0);    
+  }
+
+  glDisable(GL_DEPTH_TEST);
+}
+
 // ======== init ====================================================
 static void init( void ) {
   ARParam  wparam, cparam;   // Parametros intrinsecos de la camara
@@ -121,6 +166,10 @@ static void init( void ) {
   // Inicializamos la lista de objetos
   addObject("data/bomb.patt", 90.0, c, draw);
   addObject("data/coin.patt", 90.0, c, draw);
+  
+  // Cargamos el fichero de especificacion multimarca
+  if( (mMarker = arMultiReadConfigFile("data/marker.dat")) == NULL )
+    print_error("Error en fichero marker.dat\n");
 
   argInit(&cparam, 1.0, 0, 0, 0, 0);   // Abrimos la ventana 
 }
@@ -130,10 +179,6 @@ static void mainLoop(void) {
   ARUint8 *dataPtr;
   ARMarkerInfo *marker_info;
   int marker_num, i, j, k;
-
-  double p_width     = 90.0;        // Ancho del patron (marca)
-  // double p_width     = 120.0;        // Ancho del patron (marca)
-  double p_center[2] = {0.0, 0.0};   // Centro del patron (marca)
 
   // Capturamos un frame de la camara de video
   if((dataPtr = (ARUint8 *)arVideoGetImage()) == NULL) {
@@ -170,6 +215,10 @@ static void mainLoop(void) {
     }
   }
 
+  // Detectar patrón Multimarca
+  if(arMultiGetTransMat(marker_info, marker_num, mMarker) > 0) 
+    draw_multi();       // Dibujamos los objetos de la escena
+
   argSwapBuffers(); // Cambiamos el buffer con lo que tenga dibujado
 }
 
@@ -179,8 +228,6 @@ int main(int argc, char **argv) {
   init();                   // Llamada a nuestra funcion de inicio
   arVideoCapStart();        // Creamos un hilo para captura de video
   argMainLoop( NULL, keyboard, mainLoop );   // Asociamos callbacks para las funciones de teclado
-
-  printf("¡¡¡Bienvenido al juego de Super Mario!!!");
 
   return (0);
 }
